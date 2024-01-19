8051671e50aaef85a71586540b49c5ce783d48ef
Joshua Karpul <jkarpul@gmail.com>
2024-01-19 09:34:14 +0200

    add news card and index cards

--- script.js
+++ script.js
@@ -2,78 +2,6 @@ function isMobileDevice() {
   return /Mobi|Android/i.test(navigator.userAgent);
 }
 
-// Get the dropdown toggle elements by their unique IDs
-// var dropdownToggle1 = document.getElementById("navbarDropdown1");
-// var dropdownToggle2 = document.getElementById("navbarDropdown2");
-
-// Get the dropdown menu elements by their unique IDs
-// var dropdownMenu1 = document.getElementById("dropdownMenu1");
-// var dropdownMenu2 = document.getElementById("dropdownMenu2");
-
-// Dropdown menu 1
-// var isMenuOpen1 = false;
-// dropdownToggle1.addEventListener("mouseover", function () {
-//   if (!isMenuOpen1) {
-//     isMenuOpen1 = true;
-//     this.click();
-//   }
-// });
-// dropdownToggle1.addEventListener("click", function (event) {
-//   if (event.isTrusted) {
-//     isMenuOpen1 = !isMenuOpen1;
-//   }
-// });
-// dropdownToggle1.addEventListener("mouseleave", function (event) {
-//   var mouseY = event.clientY;
-//   var elementRect = dropdownToggle1.getBoundingClientRect();
-//   var elementBottom = elementRect.top + elementRect.height;
-
-//   if (mouseY < elementBottom && isMenuOpen1) {
-//     isMenuOpen1 = false;
-//     dropdownToggle1.click();
-//   }
-// });
-// dropdownMenu1.addEventListener("mouseleave", function (event) {
-//   var mouseY = event.clientY;
-//   var elementRect = dropdownMenu1.getBoundingClientRect();
-//   if (mouseY > elementRect.top && isMenuOpen1) {
-//     isMenuOpen1 = false
-//     dropdownToggle1.click();
-//   }
-// });
-
-// Dropdown menu 2
-// var isMenuOpen2 = false;
-// dropdownToggle2.addEventListener("mouseover", function () {
-//   if (!isMenuOpen2) {
-//     isMenuOpen2 = true;
-//     this.click();
-//   }
-// });
-// dropdownToggle2.addEventListener("click", function (event) {
-//   if (event.isTrusted) {
-//     isMenuOpen2 = !isMenuOpen2;
-//   }
-// });
-// dropdownToggle2.addEventListener("mouseleave", function (event) {
-//   var mouseY = event.clientY;
-//   var elementRect = dropdownToggle2.getBoundingClientRect();
-//   var elementBottom = elementRect.top + elementRect.height;
-
-//   if (mouseY < elementBottom && isMenuOpen2) {
-//     isMenuOpen2 = false;
-//     dropdownToggle2.click();
-//   }
-// });
-// dropdownMenu2.addEventListener("mouseleave", function (event) {
-//   var mouseY = event.clientY;
-//   var elementRect = dropdownMenu2.getBoundingClientRect();
-//   if (mouseY > elementRect.top && isMenuOpen2) {
-//     isMenuOpen2 = false
-//     dropdownToggle2.click();
-//   }
-// });
-
 /* Documentation Sidebar  */
 document.addEventListener("DOMContentLoaded", function () {
   const headerNavbar = document.querySelector('.navbar');
@@ -147,6 +75,35 @@ const news = document.getElementById('news');
 if (news) {
   news.innerHTML =`
 <div class="news-card expanded">
+  <div class="news-container">
+      <div class="news-group-item">
+          <div class="row">
+              <div class="col-auto" style="padding-right: 0">
+                  <img class="news-badge" src="../../../images/PlayBaseServices/GooglePlay_BaseServices_Plugin_Logo.png">
+              </div>
+              <div class="col" style="padding-left: 10px">
+                  <div class="news-title">New Unreal Engine Plugin</div>
+                  <div class="news-sub-title">2024-01-19</div>
+              </div>
+          </div>
+      </div>
+      
+      <div class="news-info">
+          <div class="news-info-title">
+              Play Base Services Docs Early Access
+          </div>
+          <div class="news-info-details">
+              <ul>
+                  <li>Review our Play Services Plugin</li>
+                  <li>Provide any feedback before release</li>
+                  <li>Suggestions may be included!</li>
+              </ul>
+          </div>
+          <a class="btn-learn-more" href="../../../plugins/google-play-services/base-services/overview.html?utm_source=News">Learn more</a>
+      </div>
+  </div>
+</div>
+<div class="news-card">
   <div class="news-container">
       <div class="news-group-item">
           <div class="row">
@@ -263,39 +220,6 @@ if (news) {
               </ul>
           </div>
           <a class="btn-learn-more" href="../../../plugins/google-play-services/billing/overview.html?utm_source=News">Learn more</a>
-      </div>
-  </div>
-</div>
-<div class="news-card">
-  <div class="news-container">
-      <div class="news-group-item">
-          <div class="row">
-              <div class="col-auto" style="padding-right: 0">
-                  <img class="news-badge" src="../../../images/InAppUpdates/GooglePlay_InAppUpdates_Plugin_Logo.png">
-              </div>
-              <div class="col" style="padding-left: 10px">
-                  <div class="news-title">New Unreal Engine Plugin</div>
-                  <div class="news-sub-title">2023-10-02</div>
-              </div>
-          </div>
-      </div>
-      
-      <div class="news-info">
-          <div class="row">
-              <div class="col-4">
-                  <img style="max-height: 156px; padding-top: 10px; padding-bottom: 10px" src="../../../images/general/InAppUpdates_Thumbnail.png">
-              </div>
-              <div class="col-8" style="display:flex; align-items: center; margin: 0">
-                  <div class="news-info-details" style="font-weight: 500">
-                      <ul>
-                          <li>Track Available Updates</li>
-                          <li>Immediate Required Updates</li>
-                          <li>Background Flexible Updates</li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-          <a class="btn-learn-more" href="../../../plugins/google-play-services/in-app-updates/overview.html?utm_source=News">Learn more</a>
       </div>
   </div>
 </div>

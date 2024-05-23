e434b683599f2607f0fc2bbbda8c89eea50639c5
Joshua Karpul <jkarpul@gmail.com>
2024-05-23 11:13:22 +0100

    notifications go live

--- script.js
+++ script.js
@@ -79,6 +79,34 @@ if (news) {
       <div class="news-group-item">
           <div class="row">
               <div class="col-auto" style="padding-right: 0">
+                  <img class="news-badge" src="../../../images/AndroidNotifications/AdvancedNotifications_Plugin_Logo.png">
+              </div>
+              <div class="col" style="padding-left: 10px">
+                  <div class="news-title">New Plugin Released!</div>
+                  <div class="news-sub-title">2024-05-23</div>
+              </div>
+          </div>
+      </div>
+      <div class="news-info">
+          <div class="news-info-title">
+              Advanced Mobile Notifications
+          </div>
+          <div class="news-info-details">
+              <ul>
+                  <li>Highly Customizable Notifications</li>
+                  <li>Manage Notification Permissions</li>
+                  <li>Manage Notification Channels</li>
+              </ul>
+          </div>
+          <a class="btn-learn-more" href="../../../plugins/android-native/notifications/overview.html?utm_source=News">Learn more</a>
+      </div>
+  </div>
+</div>
+<div class="news-card">
+  <div class="news-container">
+      <div class="news-group-item">
+          <div class="row">
+              <div class="col-auto" style="padding-right: 0">
                   <img class="news-badge" src="../../../images/Admob/Admob_Plugin_Logo.png">
               </div>
               <div class="col" style="padding-left: 10px">
@@ -131,7 +159,7 @@ if (news) {
       </div>
   </div>
 </div>
-<div class="news-card expanded">
+<div class="news-card">
   <div class="news-container">
       <div class="news-group-item">
           <div class="row">
@@ -213,34 +241,6 @@ if (news) {
             </div>
             <a class="btn-learn-more" href="../../../plugins/advertising/admob/overview.html?utm_source=News">Learn more</a>
         </div>
-    </div>
-</div>
-  <div class="news-card">
-    <div class="news-container">
-      <div class="news-group-item">
-          <div class="row">
-              <div class="col-auto" style="padding-right: 0">
-                  <img class="news-badge" src="../../../images/PlayBaseServices/GooglePlay_BaseServices_Plugin_Logo.png">
-              </div>
-              <div class="col-9" style="padding-left: 10px">
-                  <div class="news-title">Release Notes</div>
-                  <div class="news-sub-title">2024-03-18</div>
-              </div>
-          </div>
-      </div>
-      <div class="news-info">
-          <div class="news-info-title">
-            Play Base Services V2 Release Notes
-          </div>
-          <div class="news-info-details">
-              <ul>
-                  <li>New BP Node to Submit Score Immediatly</li>
-                  <li>New OnSuccess Delegate</li>
-                  <li>New OnFailure Delegate</li>
-              </ul>
-          </div>
-          <a class="btn-learn-more" href="../../../plugins/google-play-services/base-services/overview.html?utm_source=News">Learn more</a>
-      </div>
     </div>
 </div>
   `;
